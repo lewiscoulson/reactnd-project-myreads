e038b35f7bab59f861a1b6d6659a0336ed07c006
Lewis Coulson <lewis@potatolondon.com>
2017-07-21 10:04:26 +0100

    Change book shelf state

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
-const Book = ({title, authors, image, id, onChangeOption, shelf}) => {
-  return (
-    <div className="book">
-      <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${image})` }}></div>
-        <div className="book-shelf-changer">
-          <select value={shelf} onChange={(event) => {onChangeOption(event, id) }}>
-            <option value="">Move to...</option>
-            <option value="currentlyReading">Currently Reading</option>
-            <option value="wantToRead">Want to Read</option>
-            <option value="read">Read</option>
-            <option value="none">None</option>
-          </select>
+class Book extends Component {
+  state = {
+    shelf: ''
+  }
+
+  componentDidMount() {
+    this.setState({
+      shelf: this.props.shelf
+    })
+  }
+
+  render() {
+    const {title, authors, image, id, onChangeOption} = this.props;
+
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${image})` }}></div>
+          <div className="book-shelf-changer">
+            <select value={this.state.shelf} onChange={(event) => {onChangeOption(event, id) }}>
+              <option value="">Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
         </div>
+        <div className="book-title">{title}</div>
+        <div className="book-authors">{authors}</div>
       </div>
-      <div className="book-title">{title}</div>
-      <div className="book-authors">{authors}</div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 Book.propTypes = {
   title: PropTypes.string.isRequired,
